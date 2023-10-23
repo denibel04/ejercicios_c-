32604e8f316b7ea33e5a09e74d0cd67faf1a7f20
Denisa Belean <115071551+denibel04@users.noreply.github.com>
2023-10-23 13:02:41 +0200

    first commit

---
+++ practica1.cs
@@ -0,0 +1,25 @@
+﻿namespace Consola1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int puedeConducir = 18;
+            const string tieneCarnet = "S";
+            Console.WriteLine("Vamos a evaluar si puedes conducir, introduce tu edad");
+            int edad = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("¿Tienes carnet? (S/N)");
+            string carnet = Console.ReadLine().ToUpper();
+
+            if (edad >= puedeConducir && carnet == tieneCarnet)
+            {
+                Console.WriteLine("Puedes conducir");
+            }
+            else
+            {
+                Console.WriteLine("No puedes conducir");
+            }
+        }
+    }    
+}
---
+++ practica3.cs
@@ -0,0 +1,40 @@
+﻿namespace Practica3
+{
+    internal class Program
+    {
+        const int puedeConducir = 18;
+        const string tieneCarnet = "S";
+
+        static void Main()
+        {
+            Console.WriteLine("Vamos a evaluar si puedes conducir, introduce tu edad");
+            int edad = Int32.Parse(Console.ReadLine());
+
+            string carnet = LeerRespuesta("¿Tienes carnet? (S/N)");
+
+            if (edad >= puedeConducir && carnet == tieneCarnet)
+            {
+                Console.WriteLine("Puedes conducir");
+            }
+            else
+            {
+                Console.WriteLine("No puedes conducir");
+            }
+        }
+
+        static string LeerRespuesta(string mensaje)
+        {
+            string respuesta;
+            do
+            {
+                Console.WriteLine(mensaje);
+                respuesta = Console.ReadLine().ToUpper();
+                if (respuesta != "S" && respuesta != "N")
+                {
+                    Console.WriteLine("Se debe introducir N o S");
+                }
+            } while (respuesta != "S" && respuesta != "N");
+            return respuesta;
+        }
+    }
+}
